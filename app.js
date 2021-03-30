9acef56d843b09a142388035dbef8a2059734406
Vignesh <vignesh77024@gmail.com>
2021-03-30 17:04:48 +0530

    Canvas template is ready

---
+++ app.js
@@ -0,0 +1,25 @@
+canvas = document.querySelector("canvas")
+canvas.width = innerWidth
+canvas.height = innerHeight
+ctx = canvas.getContext("2d", {alpha: false})
+
+let centerX = canvas.width / 2
+let centerY = canvas.height / 2
+
+
+function animate() {
+	requestAnimationFrame(animate)
+	// Clear Canvas
+	ctx.fillStyle = "#000000"
+	ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+	// draw a rect
+	ctx.fillStyle = "red"
+	ctx.fillRect(centerX - 50, centerY - 50, 100, 100)
+}
+// Calling the animate function
+animate()
+// Misc. functions
+function randomBetween(min, max){
+return (Math.random() * (max - min) + min);
+}
